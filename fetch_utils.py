64670260808ffafab2fbbac371b0071eb815b800
digitalocean2345 <67731255+digitalocean2345@users.noreply.github.com>
2025-03-15 14:39:03 +0000

    created fetch_utils.py for fetch_article function

---
+++ fetch_utils.py
@@ -0,0 +1,66 @@
+import requests
+import time
+from datetime import datetime, timedelta
+
+# Function to fetch results from CSE API
+def fetch_articles(query, start_date, end_date, num_results, key, cse_id):
+    url = "https://www.googleapis.com/customsearch/v1"
+    results = []
+    start_index = 1
+
+
+    # Format the date range
+    date_range = f"{start_date}..{end_date}"  # Date format is YYYY-MM-DD..YYYY-MM-DD
+    print(date_range)
+    while len(results) < num_results:
+        params = {
+                'key': key,
+                'cx': cse_id,
+                'q': query,
+                'start': start_index, # start with the first result,
+                'dateRestrict': date_range,  # Date range
+                'sort':'date', # Use 'sort' with 'date:r' and formatted range
+                'num': 10, # Number of results per page
+            }
+
+        # Print the full URL for debugging
+        full_url = f"{url}?{'&'.join([f'{k}={v}' for k, v in params.items()])}"
+        print("Full URL:", full_url)
+
+
+        response = requests.get(url, params=params)
+        print("Response Status Code:", response.status_code)
+        #print("Response Content:", response.text)
+        print(response)
+
+        if response.status_code != 200:
+            print("Error in API request. Check the parameters and try again.")
+            break
+
+        # Check for 429 error
+        if response.status_code == 429:
+            print("Rate limit exceeded. Waiting for 60 seconds...")
+            time.sleep(60)  # Wait for 60 seconds before retrying
+            continue  # Retry the same request
+
+        data = response.json()
+        #print(data)
+
+        if 'items' in data:
+            results.extend(data['items'])
+        
+        # Check if there are more results or if we've reached the desired number
+        if "queries" in data and "nextPage" in data["queries"]:
+            # Continue to the next page if available
+            start_index += 10  # Move to the next page
+            #pass
+        else:
+            # Stop if there are no more pages or we have enough results
+            break
+
+        # Add a delay between requests to avoid hitting the rate limit
+        time.sleep(2)  # Wait for 2 seconds before the next request
+
+
+    print (f"length of results: {len(results)}")
+    return results
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import subprocess
 subprocess.run(["pip", "install", "requests", "beautifulsoup4"])
 
 from translation_utils import translate_article  # Import from translation_utils.py
+from fetch_utils import fetch_articles
 from create_archive import create_archive_index
 
 import requests
@@ -24,69 +25,6 @@ CSE_ID_English = os.getenv('CSE_ID_E')
 
 print("API_KEY:", API_KEY)
 print("CSE_ID_Chinese:", CSE_ID_Chinese)
-
-# Function to fetch results from CSE API
-def fetch_articles(query, start_date, end_date, num_results, cse_id):
-    url = "https://www.googleapis.com/customsearch/v1"
-    results = []
-    start_index = 1
-
-
-    # Format the date range
-    date_range = f"{start_date}..{end_date}"  # Date format is YYYY-MM-DD..YYYY-MM-DD
-    print(date_range)
-    while len(results) < num_results:
-        params = {
-                'key': API_KEY,
-                'cx': cse_id,
-                'q': query,
-                'start': start_index, # start with the first result,
-                'dateRestrict': date_range,  # Date range
-                'sort':'date', # Use 'sort' with 'date:r' and formatted range
-                'num': 10, # Number of results per page
-            }
-
-        # Print the full URL for debugging
-        full_url = f"{url}?{'&'.join([f'{k}={v}' for k, v in params.items()])}"
-        print("Full URL:", full_url)
-
-
-        response = requests.get(url, params=params)
-        print("Response Status Code:", response.status_code)
-        #print("Response Content:", response.text)
-        print(response)
-
-        if response.status_code != 200:
-            print("Error in API request. Check the parameters and try again.")
-            break
-
-        # Check for 429 error
-        if response.status_code == 429:
-            print("Rate limit exceeded. Waiting for 60 seconds...")
-            time.sleep(60)  # Wait for 60 seconds before retrying
-            continue  # Retry the same request
-
-        data = response.json()
-        #print(data)
-
-        if 'items' in data:
-            results.extend(data['items'])
-        
-        # Check if there are more results or if we've reached the desired number
-        if "queries" in data and "nextPage" in data["queries"]:
-            # Continue to the next page if available
-            start_index += 10  # Move to the next page
-            #pass
-        else:
-            # Stop if there are no more pages or we have enough results
-            break
-
-        # Add a delay between requests to avoid hitting the rate limit
-        time.sleep(2)  # Wait for 2 seconds before the next request
-
-
-    print (f"length of results: {len(results)}")
-    return results
 
 def publish_articles(articles):
     # Get current date
@@ -142,11 +80,11 @@ start_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 query_c = "印度"
 query_e = "india"
 # Fetch articles from Chinese sources
-articles_chinese = fetch_articles(query_c, start_date, end_date, num_results=90, cse_id=CSE_ID_Chinese)
+articles_chinese = fetch_articles(query_c, start_date, end_date, num_results=90, key=API_KEY, cse_id=CSE_ID_Chinese)
 print(f"number of articles fetched from Chinese sources: {len(articles_chinese)}")
 
 # Fetch articles from English sources
-articles_english = fetch_articles(query_e, start_date, end_date, num_results=30, cse_id=CSE_ID_English)
+articles_english = fetch_articles(query_e, start_date, end_date, num_results=30, key=API_KEY, cse_id=CSE_ID_English)
 print(f"number of articles fetched from English sources: {len(articles_english)}")
 
 # Combine the results
